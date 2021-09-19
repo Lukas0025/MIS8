a3edf84dda095000ece5217b27697472accaccca
Lukáš Plevač <lukas@plevac.eu>
2021-09-19 22:20:58 +0200

    Remove bit output from compiler

--- asm_examples/jump.asm
+++ asm_examples/jump.asm
@@ -1,15 +1,15 @@
-# Example ASM code
-# cout R0 to 100
+; Example ASM code
+; cout R0 to 100
 
-LDI A, 0 # 0,1 (2BYTES)
-LDI B, 1 # 2,3 (2BYTES) <-- Jump here
+LDI A, 0 ; 0,1 (2BYTES)
+LDI B, 1 ; 2,3 (2BYTES) <-- Jump here
 
-ADDTO A  # 4 (1BYTE)
+ADDTO A  ; 4 (1BYTE)
 
-# compare with 100
+; compare with 100
 LDI B, 100
 SUBTO C
 JNZ C, 2
 
-# counting is done
+; counting is done
 HALT
